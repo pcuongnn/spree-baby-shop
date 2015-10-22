bb99205124c4e9b7eeb5864941e2105bb57aae7d
pcuong <pcuongnn@gmail.com>
2015-10-22 11:47:36 +0700

    apply template

---
+++ config/initializers/assets.rb
@@ -0,0 +1,33 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( spree/frontend/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( spree/frontend/style.css )
+Rails.application.config.assets.precompile += %w( spree/frontend/logo.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/free.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/money.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/cart.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/fb.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/tw.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/gp.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/yt.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/visa.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l2.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l3.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l4.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l5.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l6.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l7.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l8.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l9.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/l10.png )
+Rails.application.config.assets.precompile += %w( spree/frontend/main.js )
+Rails.application.config.assets.precompile += %w( spree/frontend/responsive-tabs.js )
